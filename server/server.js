56e4427e512a0c3e3b3f1dc50bd016c843782953
MustardPenguin <andyha234@gmail.com>
2023-03-08 20:27:38 -0500

    Added packages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,29 @@
-const express = require('express');
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 const server = express();
+
+require('dotenv').config();
+
+// Set up mongoose connection
+const mongoose = require("mongoose");
+mongoose.set('strictQuery', false);
+//mongoose.set('strictPopulate', false);
+const mongoDB = process.env.mongo_url;
+
+main().catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(mongoDB);
+}
+
+server.use(logger('dev'));
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cookieParser());
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', (req, res) => res.send("Hello world!"));
 
