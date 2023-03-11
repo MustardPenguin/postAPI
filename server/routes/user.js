c983e3941c81c099658381d661f86a09729932a9
MustardPenguin <andyha234@gmail.com>
2023-03-10 23:01:42 -0500

    Account creation added

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 const router = Router();
 
@@ -10,8 +12,34 @@ router.get('/:id', (req, res) => {
     return res.send("Get user " + req.params.id);
 });
 
-router.post('/', (req, res) => {
-    return res.send("Create user");
+router.post('/', (req, res, next) => {
+    User.findOne({ username: req.body.username })
+      .then((user) => {
+        if(user) {
+            res.json({ message: "Username already exists" });
+            return;
+        }
+
+        bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+            const user = new User({
+                username: req.body.username,
+                password: hashedPassword,
+            });
+            
+            user.save().then(() => {
+                console.log("Successfully saved account");
+            }).catch((err) => {
+                return res.json(err);
+            });
+        });
+      })
+      .catch((err) => {
+        return res.json(err);
+      });
+    
+    
+
+    
 });
 
 router.delete('/:id', (req, res) => {
